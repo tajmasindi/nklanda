c5b47f974ad9f06386d5e2fe71dc733b6bef7a82
Jacques Schutte <Jacquess@bbd.co.za>
2017-03-14 16:21:26 +0200

    Added President class

---
+++ src/main/java/com/bbd/President.java
@@ -0,0 +1,9 @@
+package com.bbd;
+
+public class President extends Politician{
+    
+    public President(String n, int a, String g){
+        super(n,a,g);
+        setType(Type.PRESIDENT);
+    }
+}
