1f9e8ea492edb36ca4ca27b6f623f24ff6f08f3d
Jacques Schutte <Jacquess@bbd.co.za>
2017-03-15 10:16:30 +0200

    Setters and valid functions for Nklandla

--- src/main/java/com/bbd/Nklandla.java
+++ src/main/java/com/bbd/Nklandla.java
@@ -3,9 +3,25 @@ package com.bbd;
 public class Nklandla extends HomeStead {
     SwimmingPool swimmingPool;
     ChickenRun chickenRun;
-    Amphitheatre amphithetre;
+    Amphitheatre amphitheatre;
 
     public Nklandla(String n, String d, String p, String c) {
         super(n, d, p, c);
+    }
+
+    public void setSwimmingPool(SwimmingPool p) {
+        swimmingPool = p;
+    }
+
+    public void setChickenRun(ChickenRun c) {
+        chickenRun = c;
+    }
+
+    public void setAmphitheatre(Amphitheatre a) {
+        amphitheatre = a;
+    }
+
+    public boolean valid() {
+        return (swimmingPool != null) && (chickenRun != null) && (amphitheatre != null);
     }
 }
